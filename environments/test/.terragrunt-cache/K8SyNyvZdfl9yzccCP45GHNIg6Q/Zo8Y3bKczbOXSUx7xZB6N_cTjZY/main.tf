72c0cd7ffa8a758014758475ded9f7d21f045eab
Sebastian Tybus <sebastian.tybus@bechtle.com>
2024-04-08 16:09:51 +0200

    test

---
+++ environments/test/.terragrunt-cache/K8SyNyvZdfl9yzccCP45GHNIg6Q/Zo8Y3bKczbOXSUx7xZB6N_cTjZY/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "OIDC Test Bucket"
+    Environment = "Tesiting"
+  }
+}
